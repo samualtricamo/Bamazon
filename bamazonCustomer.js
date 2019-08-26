82ade3acb84a585786c861d99329b3bc13d01ee3
Sam Tricamo <samualtricamo@gmail.com>
2019-08-26 15:24:09 -0500

    connectivity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,9 +4,9 @@ var Table = require("cli-table");
 
 var connection = mysql.createConnection({
 	host:"localhost",
-	port:3306,
+	port:3000,
 	user:"root",
-	password:"",
+	password:"Tricamo6!",
 	database:"bamazon"
 });
 
