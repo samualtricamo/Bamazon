a38e22ecd12cba5af1fe31af11cc0a188b29080f
Sam Tricamo <samualtricamo@gmail.com>
2019-08-26 15:20:44 -0500

    stripping down code

--- bamazon.sql
+++ bamazon.sql
@@ -1,40 +1,28 @@
 DROP DATABASE IF EXISTS bamazon;
-CREATE DATABASE bamazon;
+
+CREATE database bamazon;
 
 USE bamazon;
 
-CREATE TABLE products (
-	item_id INT NOT NULL AUTO_INCREMENT,
-    product_name TEXT NOT NULL,
-    department_name TEXT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    stock_quantity INT NOT NULL,
-    product_sales DECIMAL(10, 2),
-    PRIMARY KEY (item_id)
+CREATE TABLE products(
+	item_id INT(4) NOT NULL,
+	product_name VARCHAR(100) NOT NULL,
+	department_name VARCHAR(100) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INT(20) NOT NULL,
+	PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
-('Mixing Bowls', 'Kitchen', 23.99, 23),
-('Sharpie Set', 'Office Products', 14.00, 56),
-('Bose Speaker', 'Electronics', 99.99, 200),
-('Garmin Smart Watch', 'Electronics', 269.95, 48),
-('Vinyl Record', 'Electronics', 19.49, 143),
-('Necklace', 'Bamazon Fashion', 11.99, 78),
-('Earrings', 'Bamazon Fashion', 9.99, 56),
-('Frisbee Golf Set', 'Sports & Outdoors', 35.99, 30),
-('Tent', 'Sports & Outdoors', 62.99, 77),
-('Office Printer', 'Office Products', 109.99, 60);
+Select * FROM products;
 
-CREATE TABLE departments (
-    department_id INT NOT NULL AUTO_INCREMENT,
-    department_name TEXT NOT NULL,
-    over_head_costs DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (department_id)
-);
-
-INSERT INTO departments (department_name, over_head_costs)
-VALUES ('Kitchen', 3000),
-('Office Products', 6000),
-('Electronics', 10000),
-('Bamazon Fashion', 8000),
-('Sports & Outdoors', 7000);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
+VALUES (101, "cleats", "soccer", 79.99, 20),
+	   (212, "jerseys", "basketball", 99.99, 10),
+	   (313, "helmet", "football", 29.99, 5),
+	   (420, "sweater", "hockey", 149.99, 14),
+	   (504, "pants", "football", 39.99, 15),
+	   (619, "shorts", "soccer", 19.99, 19),
+	   (720, "gloves", "baseball", 49.99, 11),
+	   (808, "bats", "baseball", 69.99, 10),
+	   (913, "sticks", "hockey", 99.99, 19),
+	   (1009, "shoes", "basketball", 99.99, 17)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,155 +1,74 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-var chalk = require('chalk');
-var Table = require('cli-table');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: '',
-    user: 'root',
-    password: 'root',
-    database: 'bamazon'
+	host:"localhost",
+	port:3306,
+	user:"root",
+	password:"",
+	database:"bamazon"
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connection successful');
-    displayItems();
+connection.connect(function(err){
+	if(err)throw err;
+	console.log("connected as id" + connection.threadId);
 });
 
-var chosenItem = {};
-
-var resetCart = function() {
-    chosenItem = {};
-}
-var displayItems = function() {
-    connection.query(`SELECT * FROM products`, (err, res) => {
-        var listTable = new Table({
-            head: ['Item ID', 'Product Name', 'Price'],
-            colWidths: [10, 45, 12]
-        });
-
-        for (var i = 0; i < res.length; i++) {
-            listTable.push([res[i].item_id, res[i].product_name, `$${res[i].price}`]);
-            // console.log(chalk.blue.bold(`\n\tItem ID: ${res[i].item_id}\n\tProduct Name: ${res[i].product_name}\n\tPrice: $${res[i].price}\n`));
-        }
-        
-        console.log(`\n\n${listTable.toString()}\n\n`);
-      
-        askForID();
-    });
-};
-
-
-var askForID = function() {
-    inquirer.prompt({
-        name: 'itemID',
-        type: 'input',
-        message: 'Enter the ID of the item you would like to purchase:',
-
-        validate: (value) => {
-            if (!isNaN(value) && (value > 0 && value <= 10)) {
-                return true;
-            } else {
-                console.log(chalk.red(' => Please enter a number from 1-10'));
-                return false;
-            }
-        }
-
-    }).then((answer) => {
-        connection.query('SELECT item_id, product_name, price, stock_quantity, product_sales FROM products WHERE ?', { item_id: answer.itemID }, (err, res) => {
-           
-            confirmItem(res[0].product_name, res);
-        });
-    });
-};
-
-
-var confirmItem = function(product, object) {
-    inquirer.prompt({
-        name: 'confirmItem',
-        type: 'confirm',
-        message: `You chose` + chalk.blue.bold(` '${product}'. `) + `Is this correct?`
-    }).then((answer) => {
-        if (answer.confirmItem) {
-            chosenItem = {
-                item_id: object[0].item_id,
-                product_name: object[0].product_name,
-                price: object[0].price,
-                stock_quantity: object[0].stock_quantity,
-                product_sales: object[0].product_sales
-            };
-     
-            askHowMany(chosenItem.item_id);
-        } else {
-            askForID();
-        }
-    });
-};
-
-
-var askHowMany = function(chosenID) {
-    inquirer.prompt({
-        name: 'howMany',
-        type: 'input',
-        message: 'How many would you like to purchase?',
-        validate: (value) => {
-            if (!isNaN(value) && value > 0) {
-                return true;
-            } else {
-                console.log(chalk.red(' => Oops, please enter a number greater than 0'));
-                return false;
-            }
-        }
-    }).then((answer) => {
-        connection.query('SELECT stock_quantity FROM products WHERE ?', { item_id: chosenItem.item_id }, (err, res) => {
-        
-            if (res[0].stock_quantity < answer.howMany) {
-                console.log(chalk.blue.bold('\n\tSorry, insufficient quantity in stock!\n'));
-              
-                inquirer.prompt({
-                    name: 'proceed',
-                    type: 'confirm',
-                    message: 'Would you still like to purchase this product?'
-                }).then((answer) => {
-                    if (answer.proceed) {
-                        askHowMany(chosenItem.item_id);
-                    } else {
-                        console.log(chalk.blue.bold('\n\tThanks for visiting! We hope to see you again soon.\n'));
-                        connection.end();
-                    }
-                });
-            } else {
-                chosenItem.howMany = answer.howMany;
-                console.log(chalk.blue.bold('\n\tOrder processing...'));
-                connection.query('UPDATE products SET ? WHERE ?', [
-                    {
-                        stock_quantity: chosenItem.stock_quantity - answer.howMany,
-                        product_sales: chosenItem.product_sales + (chosenItem.price * answer.howMany)
-                    },
-                    {
-                        item_id: chosenItem.item_id
-                    }
-                ], (err, res) => {
-                    console.log(chalk.blue.bold(`\n\tOrder confirmed!!! Your total was $${(chosenItem.price * chosenItem.howMany).toFixed(2)}.\n`));
-                       promptNewPurchase();
-                });
-            }
-        });
-    });
+var displayProducts = function(){
+	var query = "Select * FROM products";
+	connection.query(query, function(err, res){
+		if(err) throw err;
+		var displayTable = new Table ({
+			head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+			colWidths: [10,25,25,10,14]
+		});
+		for(var i = 0; i < res.length; i++){
+			displayTable.push(
+				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+				);
+		}
+		console.log(displayTable.toString());
+		purchasePrompt();
+	});
 }
 
-var promptNewPurchase = function() {
-    inquirer.prompt({
-        name: 'newPurchase',
-        type: 'confirm',
-        message: 'Would you like to make another purchase?'
-    }).then((answer) => {
-        if (answer.newPurchase) {
-            resetCart();
-            askForID();
-        } else {
-            console.log(chalk.blue.bold('\n\tWe appreciate your business. Have a great day!\n'));
-            connection.end();
-        }
-    });
+function purchasePrompt(){
+	inquirer.prompt([
+	{
+		name: "ID",
+		type: "input",
+		message:"Please enter Item ID you like to purhcase.",
+		filter:Number
+	},
+	{
+		name:"Quantity",
+		type:"input",
+		message:"How many items do you wish to purchase?",
+		filter:Number
+	},
+
+ ]).then(function(answers){
+ 	var quantityNeeded = answers.Quantity;
+ 	var IDrequested = answers.ID;
+ 	purchaseOrder(IDrequested, quantityNeeded);
+ });
 };
+
+function purchaseOrder(ID, amtNeeded){
+	connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
+		if(err){console.log(err)};
+		if(amtNeeded <= res[0].stock_quantity){
+			var totalCost = res[0].price * amtNeeded;
+			console.log("Good news your order is in stock!");
+			console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
+
+			connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+		} else{
+			console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+		};
+		displayProducts();
+	});
+};
+
+displayProducts(); 
